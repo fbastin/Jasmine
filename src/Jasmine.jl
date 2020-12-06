07c79315e83f1edc7a80e3aee803b81ac83528c4
fbastin <fabian.bastin@gmail.com>
2020-12-05 21:42:07 -0500

    Initial public release.

---
+++ src/Jasmine.jl
@@ -0,0 +1,16 @@
+module Jasmine
+
+using LinearAlgebra, Combinatorics
+
+import Base.println
+
+
+export AbstractStandard, BaseEnumeration, StandardSimplexe, solve!,  Simplexe,
+     AbstractStatus, Optimal, Infeasible, Unknown, Unbounded,
+     xstar, vstar
+
+include("utils.jl")
+include("Status.jl")
+include("Standard/main.jl")
+
+end # module
